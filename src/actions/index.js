e78855bbeb033bc659afa989126324355ffaf6bd
ypedro <ynoa.pedro@outlook.com>
2019-01-22 23:31:07 -0300

    Creating the actions

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import *  as types from '../constants/ActionTypes'
+
+let nextMessageID = 0;
+const nextUserId = 0;
+
+export const addMessage = (message, author) = ({
+    type: types.ADD_MESSAGE,
+    id: nextMessageID++,
+    message,
+    author,
+})
+
+export const addUser = name => ({
+    type: types.ADD_USER,
+    id: nextUserId++,
+    name,
+})
+
+export const messageReceived = (message, author) =>({
+    type: types.MESSAGE_RECEIVED,
+    id: nextMessageID++, 
+    message,
+    author,
+})
+
+export const populateUsersList = users => ({
+    type: types.USER_LIST,
+    users,
+})
---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_MESSAGE = "ADD_MESSAGE";
+export const ADD_MESSAGE = "ADD_MESSAGE";
+export const MESSAGE_RECEIVED = "MESSAGE_RECEIVED";
+export const ADD_USER = "ADD_USER";
+export const USER_LIST = "USER_LIST";
