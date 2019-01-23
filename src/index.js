f541047ceafcd4aaca60211ee3347f9a46b0299c
ypedro <ynoa.pedro@outlook.com>
2019-01-22 23:23:37 -0300

    Adding empty components and the redux import into index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import chat from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const store = createStore(chat);
+
+ReactDOM.render(
+    <provider store={store}>
+    <App />
+    </provider>, document.getElementById('root'));
 serviceWorker.unregister();
